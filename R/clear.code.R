aa229cb45f11be4bb3c88a1a26371ba8bda17564
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-11-29 14:51:37 -0600

    New function

---
+++ R/clear.code.R
@@ -0,0 +1,29 @@
+#' Creates a file copying current code without comments and tabuations
+#'
+#' @return clear code to be executed
+#' @export
+#' 
+#' @importFrom utils read.table
+#' @importFrom rstudioapi getActiveDocumentContext
+#' @importFrom utils head
+#'
+#' @examples
+#'
+#'#' \donttest{
+#' # Copy and paste all the following code in RStudio
+#' }
+#' 1+1
+#' 
+#' clear.code()
+#'
+clear.code <- function(){
+    ctx<-try(rstudioapi::getActiveDocumentContext(),silent=TRUE)
+    if (!inherits(ctx,"try-error")){
+        contenido<-ctx[["contents"]]
+        file_w <- file("new.R")
+        writeLines(((contenido)),file_w)
+        writeLines(as.character(parse("new.R")),file_w)
+        file.edit("new.R")
+    }
+    return(invisible())
+}
